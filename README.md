ab24d7fdb58befd7fc5e06881ef5899f4a68fba5
Baran Cezayirli <barancezayirlioglu@gmail.com>
2016-10-05 15:17:36 +0300

    eslint react & implementation of react

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# materialized-react
-React-redux boilerplate with Babel, testing, coverage and example app
+# Materialized React
+React-redux boilerplate with Babel, testing, coverage and example app.
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './favicon.ico';
+
+const App = () => (
+  <p>Under construction</p>
+);
+
+render(<App />, document.getElementById('app'));
